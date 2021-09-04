bd48e3b8e96a7b5b6b48d96e569980cd3be4ec9a
llgava <llgavamt@gmail.com>
2021-09-03 21:54:54 -0400

    Update kick slash command example

--- examples/commands/kick.ts
+++ examples/commands/kick.ts
@@ -3,11 +3,12 @@ import { SlashCommand } from 'seface-kit';
 
 export const command: SlashCommand = {
   name: 'kick',
-  description: 'kick a user',
+  description: 'Kick a user from the server.',
   options: [
-    { name: 'user', description: 'user to be kicked', type: 6, required: true },
-    { name: 'reason', description: 'reason for the kick', type: 3 },
+    { name: 'user', description: 'User to be kicked.', type: 6, required: true },
+    { name: 'reason', description: 'Reason for the kick.', type: 3 },
   ],
+  guilds: '910847586061986462',
   isSlashCommand: true,
   execute: async (client, interaction: CommandInteraction) => {
     const optUser: User = interaction.options.getUser('user');
@@ -18,7 +19,7 @@ export const command: SlashCommand = {
 
     if (sender.permissions.has('KICK_MEMBERS')) {
       if (!user.kickable) {
-        return interaction.reply(`You cannot kick user ${user.displayName}`);
+        return interaction.reply(`You cannot kick the user ${user.displayName}`);
       }
 
       user.kick(optReason);
