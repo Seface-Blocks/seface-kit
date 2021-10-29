421640268a9e49d5cc1c7f39fd8bc87cd2be9c85
llgava <llgavamt@gmail.com>
2021-10-29 15:58:33 -0400

    Add comment on Utils

--- src/common/utils/Utils.ts
+++ src/common/utils/Utils.ts
@@ -1,5 +1,6 @@
 class Utils {
   
+  /** Return a boolean values if the current file has some of extensions. */
   public checkFileExtension(file: string, extensions: string[]): boolean {
     if(extensions.includes(file.substr(file.lastIndexOf('.')))) {
       return true;
